f743c9342aeda1c4a4c04f9134808a84c27811bd
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2025-10-13 02:10:56 +0530

    ❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳

--- config.py
+++ config.py
@@ -40,6 +40,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+#isko delete karna hai koi Kam Nahin hai iska ❌️
 MIN_CHANNEL_ID = -1002306761363
 
 
